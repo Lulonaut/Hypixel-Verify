1b7f51caf4920fb81125cfefe8824a19531661af
Lulonaut <Lulonaut@users.noreply.github.com>
2020-08-05 10:41:18 +0200

    changed some stuff

--- Bot.py
+++ Bot.py
@@ -4,11 +4,12 @@ from discord.utils import get
 from Functions import getdiscord, key
 
 
-
+#Discord Key used to run the Bot
 KEY = key.key()
-PREFIX = "-"
-#assigned later
-ROLE = None
+#The Prefix of the Bot
+PREFIX = "v!"
+#The Role it gives People
+ROLE = "Hypixel Verified"
 
 # Sets Command Prefix and removes existing #help command
 client = commands.Bot(command_prefix = PREFIX)
@@ -16,19 +17,12 @@ client.remove_command("help")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(f"{PREFIX}help"))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("sendhelp"))
     print("Ready")
 
-@client.command(aliases=["start", "setup"])
-@commands.has_permissions(manage_messages=True)
-async def init(ctx, role):
-    global ROLE
-    ROLE = role
-    await ctx.send(f"Setup Complete. The Bot will now give the Role `{ROLE}` to everyone that verifys. Please make sure this Role exists!")
 
 
-
-@client.command(aliases=["v"])
+@client.command(aliases=["verifyme"])
 async def verify(ctx, name):
     #defines Member
     member = ctx.message.author
@@ -54,20 +48,8 @@ async def verify(ctx, name):
 
     elif Output2 != member2 and ROLE != None:
         await ctx.send(f"The current Discord set on the Account `{Output}` doesnt match your discord name `{member}`. If you just changed wait a few minutes and try again.")
-        
-    else:
-        await ctx.send(f"Please setup the Bot First with `{PREFIX}setup (role)` or make sure your role exists and can be assigned! ")
-
-
     
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def prefix(ctx, prefix):
-    global PREFIX
-    PREFIX = prefix
-    client.command_prefix = PREFIX
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(f"{PREFIX}help"))
-    await ctx.send(f"The new Prefix is now {PREFIX}")
+    else:
+        await ctx.send("Error, maybe try again in a bit")
 
 client.run(KEY)
