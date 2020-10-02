60e324b308debace184e4c7671743788059db73b
Lulonaut <lars.richter@gmail.com>
2020-10-02 18:51:07 +0200

    requirement check

--- Functions/requesthandler.py
+++ Functions/requesthandler.py
@@ -1,26 +1,39 @@
-# https://sky.shiiyu.moe/api/v2/profile/Lulonaut
-# https://chrome.google.com/webstore/detail/json-handle/iahnhfdhidomcpggpaimmmahffihkfnj
-
-# TODO: Check active Profile, check Skill avg without progression, check slayers
-# Slayers :https://sky.shiiyu.moe/api/v2/slayers/Lulonaut
-# Use active Profile and search for total SlayerXP
-from Functions import storage
+import storage
 import requests
-from Functions import storage
 
-finalprofile = None
+# TODO: Altes Monitor Kabel bei neuem verwenden-> NVIDIA Control Panel -> Change resolution -> Output Dynamic range
 
 def fetch(url):
     return (lambda u: requests.get(url).json())(url)
 
-#Only for testing!
+# Only for testing!
+
+
 def storecurrentdata(name):
     data = fetch(f"https://sky.shiiyu.moe/api/v2/profile/{name}")
 
     storage.savetojson(data)
 
 
-# TODO: implement it returning both slayers and skill avg.
+def tryhardyesno(name):
+    data = fetch(f"https://sky.shiiyu.moe/api/v2/profile/{name}")
+
+    data = data['profiles']
+
+    keys = list(data.keys())
+    tosearch = []
+    for i in range(5):
+        try:
+            tosearch.append(keys[i])
+        except IndexError:
+            pass
+    for i in tosearch:
+        if data[i]['current'] == True:
+            finalprofile = i
+            pass
+
+
+# TODO: return true/false for meeting reqs (see tryhardyesno())
 def skillaverage(name):
     data = fetch(f"https://sky.shiiyu.moe/api/v2/profile/{name}")
 
@@ -37,12 +50,37 @@ def skillaverage(name):
         if data[i]['current'] == True:
             global finalprofile
             finalprofile = i
+            pass
     return data[finalprofile]['data']['average_level_no_progress']
 
 
 def slayers(name):
     slayerdata = fetch(f"http://sky.shiiyu.moe/api/v2/slayers/{name}")
     slayerdata = slayerdata['profiles'][finalprofile]
+    #slayerdata = storage.loadfromjson()
+    sven = slayerdata['slayers']['wolf']['level']['currentLevel']
+    tara = slayerdata['slayers']['spider']['level']['currentLevel']
+    rev = slayerdata['slayers']['zombie']['level']['currentLevel']
 
-    
+    lvl7 = 0
+    sven = int(sven)
+    tara = int(tara)
+    rev = int(rev)
+    if sven > 6:
+        lvl7 = lvl7+1
 
+    if tara > 6:
+        lvl7 = lvl7+1
+
+    if rev > 6:
+        lvl7 = lvl7+1
+
+    if lvl7 >= 2:
+        return True
+    else:
+        return False
+
+
+testsubject = "Lulonaut"
+print(skillaverage(testsubject))
+print(slayers(testsubject))
